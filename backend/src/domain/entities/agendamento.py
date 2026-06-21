39dc545c427aeca5370ea358bed0e780360998f1
Victor Gatti <viniciusgattirodrigues2003@gmail.com>
2026-06-20 21:29:25 -0300

    refactor(domain): substitui magic strings por Enum em StatusAgendamento

--- backend/src/domain/entities/agendamento.py
+++ backend/src/domain/entities/agendamento.py
@@ -1,5 +1,13 @@
 from datetime import datetime, timedelta
 from typing import List
+from enum import Enum
+
+# Criação de Enum para otimização
+class StatusAgendamento(Enum):
+    PENDENTE = "PENDENTE"
+    CONFIRMADO = "CONFIRMADO"
+    CONCLUIDO = "CONCLUIDO"
+    CANCELADO = "CANCELADO"
 
 class Agendamento:
     def __init__(self, id_agendamento: str, cliente_id: str, data_hora_agendada: datetime, servicos: List[str]):
@@ -7,7 +15,7 @@ class Agendamento:
         self.cliente_id = cliente_id
         self.data_hora_agendada = data_hora_agendada
         self.servicos = servicos
-        self.status = "PENDENTE" # Ciclo : PENDENTE -> CONFIRMADO -> CONCLUIDO ou CANCELADO
+        self.status = StatusAgendamento.PENDENTE # Ciclo : PENDENTE -> CONFIRMADO -> CONCLUIDO ou CANCELADO / Atualizado para utilizar o Enum
         self.criado_em = datetime.now()
 
     def pode_ser_alterado_pelo_cliente(self, data_atual_da_requisicao: datetime) -> bool:
@@ -15,16 +23,17 @@ class Agendamento:
         return tempo_restante >= timedelta(days=2) # True >= 48h Agenda | False < 48h Ligar no salão
         
     def confirmar(self):
-        if self.status == "CANCELADO":
+        # Comparações atualizadas para usar Enum
+        if self.status == StatusAgendamento.CANCELADO:
             raise ValueError("Não é possível confirmar um agendamento cancelado.")
-        self.status = "CONFIRMADO"
+        self.status = StatusAgendamento.CONFIRMADO
 
     def concluir(self):
-        if self.status != "CONFIRMADO":
+        if self.status != StatusAgendamento.CONFIRMADO:
             raise ValueError("O agendamento precisa estar confirmado antes de ser concluído.")
-        self.status = "CONCLUIDO"
+        self.status = StatusAgendamento.CONCLUIDO
 
     def cancelar(self):
-        if self.status == "CONCLUIDO":
+        if self.status == StatusAgendamento.CONCLUIDO:
             raise ValueError("Não é possível cancelar um serviço que já foi concluído.")
-        self.status = "CANCELADO"
+        self.status = StatusAgendamento.CANCELADO
